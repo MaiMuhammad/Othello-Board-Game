1ac6b7fcc3a24f618ee260ccbd2b88dea61548e6
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2024-05-13 23:51:38 +0300

    editing gui

--- Board.py
+++ Board.py
@@ -1,26 +1,25 @@
 class Board:
     def __init__(self):
         self.board = [[' ' for _ in range(8)] for _ in range(8)]
-        self.board[3][3] = 'W'
-        self.board[3][4] = 'B'
-        self.board[4][3] = 'B'
-        self.board[4][4] = 'W'
+        self.colors = [[' ' for _ in range(8)] for _ in range(8)]
+        self.colors[3][3] = 'W'
+        self.colors[3][4] = 'B'
+        self.colors[4][3] = 'B'
+        self.colors[4][4] = 'W'
+
+
 
     def __getitem__(self, key):
-        return self.board[key]
+        return self.colors[key]
 
     def flip(self, row, col):
-        if self[row][col] == 'W':
-            self[row][col] = 'B'
+        if self.colors[row][col] == 'W':
+            self.colors[row][col] = 'B'
         else:
-            self[row][col] = 'W'
-
-    def flip_in_between(self):
-        # To be implemented
-        pass
+            self.colors[row][col] = 'W'
 
     def is_full(self):
-        for row in self.board:
+        for row in self.colors:
             for cell in row:
                 if cell == ' ':
                     return False
--- GUI.py
+++ GUI.py
@@ -10,44 +10,73 @@ class OthelloGUI:
 
     def create_board(self):
         self.cells = [[None for _ in range(8)] for _ in range(8)]
-        valid_moves = self.game.current_player.findValidMoves(self.game.board)
+        valid_moves = self.game.current_player.findValidMoves(self.game.board.colors)
         for i in range(8):
             for j in range(8):
-                cell = tk.Label(self.master, text=self.game.board[i][j], width=8, height=4, bg='green', relief=tk.RIDGE)
+                cell = tk.Canvas(self.master, width=60, height=60, bg='green', highlightthickness=1,
+                                 highlightbackground='black')
                 cell.grid(row=i, column=j)
                 if (i, j) in valid_moves:
-                    cell.config(bg='light green')
-                cell.bind('<Button-1>', lambda e, row=i, col=j: self.handle_click(row, col))
+                    cell.config(bg='dark green')
+                    cell.bind('<Button-1>', lambda e, row=i, col=j: self.handle_click(row, col))
                 self.cells[i][j] = cell
 
+        self.draw_filled_circle('W', 3, 3)
+        self.draw_filled_circle('B', 4, 3)
+        self.draw_filled_circle('B', 3, 4)
+        self.draw_filled_circle('W', 4, 4)
+
     def handle_click(self, row, col):
-        if self.game.board[row][col] == ' ':
-            valid_moves = self.game.current_player.findValidMoves(self.game.board)
+        if self.game.board.colors[row][col] == ' ':
+
+            valid_moves = self.game.current_player.findValidMoves(self.game.board.colors)
             if (row, col) in valid_moves:
-                self.game.board[row][col] = str(self.game.current_player.color)
-                self.game.current_player.flip(row,col,self.game.board)
+                self.game.board.colors[row][col] = str(self.game.current_player.color)
+                self.draw_filled_circle(self.game.current_player.color, row, col)
+                self.game.current_player.flip(row, col, self.game.board.colors)
                 self.game.switch_player()
-                self.update_board()
+                self.update_board()  # Update the board after each move
 
     def update_board(self):
+
         for i in range(8):
             for j in range(8):
                 cell = self.cells[i][j]
-                if self.game.board[i][j] == ' ':
-                    valid_moves = self.game.current_player.findValidMoves(self.game.board)
+                valid_moves = self.game.current_player.findValidMoves(self.game.board.colors)
+                if self.game.board.colors[i][j] == ' ':
                     if (i, j) in valid_moves:
-                        cell.config(bg='light green')
+                        cell.config(bg='dark green')
+                        cell.bind('<Button-1>', lambda e, row=i, col=j: self.handle_click(row, col))
                     else:
-                        cell.config(text=' ', bg='green')
+                        cell.config(bg='green')
+                    cell.delete("circle")
                 else:
-                    cell.config(text=self.game.board[i][j], bg='green')
+                    cell.config(bg='green')
+
+                    self.draw_filled_circle(self.game.board.colors[i][j], i, j)
+
+        # After updating the board, check if the game is over
+        if self.game.game_over():
+            self.show_message()
+
+    def draw_filled_circle(self, color, x, y):
+        if color == "W":
+            circle_color = "white"
+        else:
+            circle_color = "black"
+        cell_size = 60
+        circle_radius = cell_size // 2
+        canvas = self.cells[x][y]
+        canvas.create_oval(2, 2, cell_size - 2, cell_size - 2, outline='black', width=1, fill=circle_color)
+
 
     def show_message(self):
         if self.game.game_over():
-            if self.game.player2.count_score(self.game.board) == self.game.player1.count_score(self.game.board):
+            if self.game.player2.count_score(self.game.board.colors) == self.game.player1.count_score(
+                    self.game.board.colors):
                 message = 'Game over!\nDraw!'
             else:
-                message = f"Game over! {'Black' if self.game.player1.count_score(self.game.board) > self.game.player2.count_score(self.game.board) else 'White'} wins!"
+                message = f"Game over! {'Black' if self.game.player1.count_score(self.game.board.colors) > self.game.player2.count_score(self.game.board.colors) else 'White'} wins!"
         else:
             message = 'Game is still in progress'
 
@@ -60,3 +89,5 @@ class OthelloGUI:
 
         ok_button = tk.Button(popup, text="OK", command=popup.destroy, width=6, height=1)
         ok_button.pack(pady=8)
+
+        self.master.wait_window(popup)
