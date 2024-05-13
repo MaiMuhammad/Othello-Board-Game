a01390755e3826008f74eecac51589de25de1578
fatmaabdelmonem <fatmaabdelmonem03@gmail.com>
2024-05-14 01:10:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from Game import Game
 from Player import Player
 from Board import Board
 from GUI import OthelloGUI
+from Computer import Computer
 
 if __name__ == "__main__":
 
     Player1 = Player('B')
-    Player2 = Player('W')
+    Player2 = Computer('W')
     root = tk.Tk()
     board = Board()
     game = Game(board, Player1, Player2)
