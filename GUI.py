af67a07e1693f298eb75c2e535dba7327834e6ce
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2024-05-14 13:04:15 +0300

    editing gui

--- GUI.py
+++ GUI.py
@@ -9,15 +9,46 @@ class OthelloGUI:
         self.create_board()
 
     def create_board(self):
+        self.master.configure(bg='gray14')
         self.cells = [[None for _ in range(8)] for _ in range(8)]
         valid_moves = self.game.current_player.findValidMoves(self.game.board.colors)
+
+        # Create a frame to hold the score labels
+        score_frame = tk.Frame(self.master, bg='gray22')
+        score_frame.grid(row=0, column=0, columnspan=100, sticky="ew", padx=80, pady=20)
+
+
+        # Frame for player 1's score
+        player1_frame = tk.Frame(score_frame,bg='gray22')
+        player1_frame.pack(side=tk.LEFT, anchor=tk.W,pady=30,padx=30)  # Adjust padx here
+
+        # Left-aligned label for player 1's score
+        self.player1_score = tk.StringVar()
+        player1_label = tk.Label(player1_frame, text='Player 1\n', font=('Arial', 14, 'bold'), fg='sea green',bg='gray22')
+        player1_label.pack(side=tk.TOP, anchor=tk.W)
+
+        player1_score_label = tk.Label(player1_frame, textvariable=self.player1_score, font=('Arial', 12,), padx=10,fg='white',bg='gray22')
+        player1_score_label.pack(side=tk.TOP,anchor=tk.W)
+
+        # Frame for player 2's score
+        player2_frame = tk.Frame(score_frame,bg='gray22')
+        player2_frame.pack(side=tk.RIGHT, anchor=tk.E, padx=(5, 0))
+
+        # Right-aligned label for player 2's score
+        self.player2_score = tk.StringVar()
+        player2_label = tk.Label(player2_frame, text='Player 2\n', font=('Arial', 14, 'bold'),padx=30, fg='sea green',bg='gray22')
+        player2_label.pack(side=tk.TOP, anchor=tk.E)
+
+        player2_score_label = tk.Label(player2_frame, textvariable=self.player2_score, font=('Arial', 12), padx=30,anchor='center',fg='white',bg='gray22')
+        player2_score_label.pack(side=tk.TOP)
+
         for i in range(8):
             for j in range(8):
-                cell = tk.Canvas(self.master, width=60, height=60, bg='green', highlightthickness=1,
+                cell = tk.Canvas(self.master, width=55, height=55, bg='sea green', highlightthickness=1,
                                  highlightbackground='black')
-                cell.grid(row=i, column=j)
+                cell.grid(row=i + 1, column=j)
                 if (i, j) in valid_moves:
-                    cell.config(bg='dark green')
+                    cell.config(bg='dark sea green')
                     cell.bind('<Button-1>', lambda e, row=i, col=j: self.handle_click(row, col))
                 self.cells[i][j] = cell
 
@@ -25,6 +56,13 @@ class OthelloGUI:
         self.draw_filled_circle('B', 4, 3)
         self.draw_filled_circle('B', 3, 4)
         self.draw_filled_circle('W', 4, 4)
+
+        # Update the scores initially
+        self.update_scores()
+
+    def update_scores(self):
+        self.player1_score.set(f"Color: {self.game.player1.color}\nScore: {self.game.player1.count_score(self.game.board.colors)}")
+        self.player2_score.set(f"Color: {self.game.player2.color}\nScore: {self.game.player2.count_score(self.game.board.colors)}")
 
     def handle_click(self, row, col):
         if self.game.board.colors[row][col] == ' ':
@@ -45,15 +83,18 @@ class OthelloGUI:
                 valid_moves = self.game.current_player.findValidMoves(self.game.board.colors)
                 if self.game.board.colors[i][j] == ' ':
                     if (i, j) in valid_moves:
-                        cell.config(bg='dark green')
+                        cell.config(bg='dark sea green')
                         cell.bind('<Button-1>', lambda e, row=i, col=j: self.handle_click(row, col))
                     else:
-                        cell.config(bg='green')
+                        cell.config(bg='sea green')
                     cell.delete("circle")
                 else:
-                    cell.config(bg='green')
+                    cell.config(bg='sea green')
 
                     self.draw_filled_circle(self.game.board.colors[i][j], i, j)
+
+        # Update the scores initially
+        self.update_scores()
 
         # After updating the board, check if the game is over
         if self.game.game_over():
@@ -64,10 +105,10 @@ class OthelloGUI:
             circle_color = "white"
         else:
             circle_color = "black"
-        cell_size = 60
+        cell_size = 55
         circle_radius = cell_size // 2
         canvas = self.cells[x][y]
-        canvas.create_oval(2, 2, cell_size - 2, cell_size - 2, outline='black', width=1, fill=circle_color)
+        canvas.create_oval(4, 4, cell_size - 4, cell_size - 4, outline='black', width=1, fill=circle_color)
 
 
     def show_message(self):
--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 class Game:
 
+
     def __init__(self, board, player1, player2):
         self.board = board
         self.player1 = player1
